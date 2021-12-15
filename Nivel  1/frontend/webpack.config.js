ee7394e1b206aabca5a7b57b31b1c8d51583eb80
Anderson Barros Silva <anderson.tec12@gmail.com>
2021-12-15 06:18:36 -0300

    configs babel and webpack with live reload

---
+++ Nivel  1/frontend/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const hetmlWebPackPlugin = require("html-webpack-plugin");
+
+const isDevelopment = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  mode: isDevelopment ? "development" : "production",
+  devtool: isDevelopment ? "eval-source-map" : "source-map",
+
+  entry: path.resolve(__dirname, "src", "index.jsx"),
+  devServer: {
+    compress: true,
+    port: 9000,
+    allowedHosts: "all",
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    // contentBase: path.resolve(__dirname, "public"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  plugins: [
+    new hetmlWebPackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+};
