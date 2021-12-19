5c90b1e35ffe793ce29139a7baa242fc744e4266
Anderson Barros Silva <anderson.tec12@gmail.com>
2021-12-19 13:04:55 -0300

    types in to array

--- Nivel  1/typescript/src/routes.ts
+++ Nivel  1/typescript/src/routes.ts
@@ -1,9 +1,20 @@
-import {Request,Response} from 'express'
+import { Request, Response } from "express";
 
-import createUser from './services/CreateUser'
+import createUser from "./services/CreateUser";
 
-export function helloWorld(request:Request,response:Response){
-    const user = createUser({email:'anderson@gmail.com',name:'Anderson B. Silva',password:'123'})
+export function helloWorld(request: Request, response: Response) {
+  const user = createUser({
+    email: "anderson@gmail.com",
+    name: "Anderson B. Silva",
+    password: "123",
+    techs: [
+      "Javascript",
+      {
+        title: "React",
+        experience: 80,
+      },
+    ],
+  });
 
-    return response.json(user)
+  return response.json(user);
 }
--- Nivel  1/typescript/src/services/CreateUser.ts
+++ Nivel  1/typescript/src/services/CreateUser.ts
@@ -1,14 +1,28 @@
+type EechsProps = {
+  title: string;
+  experience: number;
+};
 
 type createUSerType = {
-    name?:string,
-    email:string,
-    password:string
-}
+  name?: string;
+  email: string;
+  password: string;
+  techs: Array<string | EechsProps>;
+  //   techs: EechsProps[] | string[];
+};
 
-export default function createUser({email,name = "",password}:createUSerType){
-    const user:createUSerType={
-        name,email,password
-    }
+export default function createUser({
+  email,
+  name = "",
+  password,
+  techs,
+}: createUSerType) {
+  const user: createUSerType = {
+    name,
+    email,
+    password,
+    techs,
+  };
 
-    return user
+  return user;
 }
