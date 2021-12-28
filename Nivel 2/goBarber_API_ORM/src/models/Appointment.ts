efd940788b0601e5c40f71028cf437c60f0b9d8e
Anderson Barros Silva <anderson.tec12@gmail.com>
2021-12-28 08:52:50 -0300

    refactory model appointments

--- Nivel 2/goBarber_API_ORM/src/models/Appointment.ts
+++ Nivel 2/goBarber_API_ORM/src/models/Appointment.ts
@@ -1,16 +1,23 @@
-import { uuid } from "uuidv4";
+// import { uuid } from "uuidv4";
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
 
+@Entity("appointments")
 class Appointment {
+  @PrimaryGeneratedColumn("uuid")
   id: string;
+
+  @Column()
   provider: string;
+
+  @Column("time with time zone")
   date: Date;
 
   //provider: string, date: Date
-  constructor({ date, provider }: Omit<Appointment, "id">) {
-    this.provider = provider;
-    this.date = date;
-    this.id = uuid();
-  }
+  // constructor({ date, provider }: Omit<Appointment, "id">) {
+  //   // this.provider = provider;
+  //   // this.date = date;
+  //   // this.id = uuid();
+  // }
 }
 
 export default Appointment;
