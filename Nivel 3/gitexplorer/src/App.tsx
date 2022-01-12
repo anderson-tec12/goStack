6b15587ffadde59cdd6b681b2fd2ad5319be5589
Anderson Barros Silva <anderson.tec12@gmail.com>
2022-01-12 09:21:49 -0300

    add react-router-dom to routes in app

--- Nivel 3/gitexplorer/src/App.tsx
+++ Nivel 3/gitexplorer/src/App.tsx
@@ -1,5 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./routes";
+
 function App() {
-  return <h1>Test</h1>;
+  return (
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ Nivel 3/gitexplorer/src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { Switch, Route } from "react-router-dom";
+
+import Repository from "../pages/Repository";
+import Dashboard from "../pages/Dashboard";
+
+const Routes = () => (
+  <Switch>
+    <Route path="/" exact component={Dashboard} />
+    <Route path="/repository" component={Repository} />
+  </Switch>
+);
+
+export default Routes;
