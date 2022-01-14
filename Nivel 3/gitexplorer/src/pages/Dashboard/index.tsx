2d4bdf4d07c40a9334948ce2fa46639d650d7795
Anderson Barros Silva <anderson.tec12@gmail.com>
2022-01-14 10:16:46 -0300

    styles finished dashboard page

--- Nivel 3/gitexplorer/src/pages/Dashboard/index.tsx
+++ Nivel 3/gitexplorer/src/pages/Dashboard/index.tsx
@@ -1,10 +1,32 @@
 import React from "react";
-import { Title, Container } from "./styles";
+import logoImg from "../../assets/logo.svg";
+import { FiChevronRight } from "react-icons/fi";
+import { Title, Container, Form, Repositories } from "./styles";
 
 const Dashboard: React.FC = () => {
   return (
     <Container>
+      <img src={logoImg} alt="Github log" />
       <Title>Explore repositórios no Github</Title>
+      <Form action="">
+        <input type="text" placeholder="Digite o nome do repositorio" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="teste">
+          <img
+            src="http://placeimg.com/640/480/city"
+            alt="repositorio qualquer"
+          />
+
+          <div>
+            <strong>Anderson/goStack</strong>
+            <p>Estudando</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </Container>
   );
 };
