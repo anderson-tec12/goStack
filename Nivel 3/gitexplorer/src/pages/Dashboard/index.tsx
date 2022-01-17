107d935e0bd40c0fbc5c225d163feba7b6fba276
Anderson Barros Silva <anderson.tec12@gmail.com>
2022-01-17 12:40:16 -0300

    conect front with github api

--- Nivel 3/gitexplorer/src/pages/Dashboard/index.tsx
+++ Nivel 3/gitexplorer/src/pages/Dashboard/index.tsx
@@ -1,32 +1,68 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
 import logoImg from "../../assets/logo.svg";
+import { api } from "../../services/api";
 import { FiChevronRight } from "react-icons/fi";
 import { Title, Container, Form, Repositories } from "./styles";
 
+/*
+  rocketseat/unform
+  facebook/react
+*/
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState("");
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepositories(
+    e: FormEvent<HTMLFormElement>
+  ): Promise<void> {
+    e.preventDefault();
+
+    const { data } = await api.get<Repository>(`repos/${newRepo}`);
+
+    console.log(data);
+    setRepositories([...repositories, data]);
+    setNewRepo("");
+  }
+
+  function renderRepositories(repository: Repository) {
+    return (
+      <a href="teste" key={repository.full_name}>
+        <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+
+        <div>
+          <strong>{repository.full_name}</strong>
+          <p>{repository.description}</p>
+        </div>
+
+        <FiChevronRight size={20} />
+      </a>
+    );
+  }
+
   return (
     <Container>
       <img src={logoImg} alt="Github log" />
       <Title>Explore repositórios no Github</Title>
-      <Form action="">
-        <input type="text" placeholder="Digite o nome do repositorio" />
+      <Form onSubmit={handleAddRepositories}>
+        <input
+          type="text"
+          placeholder="Digite o nome do repositorio"
+          value={newRepo}
+          onChange={({ target }) => setNewRepo(target.value)}
+        />
         <button type="submit">Pesquisar</button>
       </Form>
-      <Repositories>
-        <a href="teste">
-          <img
-            src="http://placeimg.com/640/480/city"
-            alt="repositorio qualquer"
-          />
-
-          <div>
-            <strong>Anderson/goStack</strong>
-            <p>Estudando</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-      </Repositories>
+      <Repositories>{repositories.map(renderRepositories)}</Repositories>
     </Container>
   );
 };
