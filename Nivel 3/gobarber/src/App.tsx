7cdbe62a217d632e3f5cc17a70d2000ccebd8354
Anderson Barros Silva <anderson.tec12@gmail.com>
2022-01-20 10:43:56 -0300

    gobarber: styles globlas

--- Nivel 3/gobarber/src/App.tsx
+++ Nivel 3/gobarber/src/App.tsx
@@ -1,5 +1,11 @@
+import GlobalStyles from "./styles/global";
 function App() {
-  return <h1>GoBarber</h1>;
+  return (
+    <>
+      <h1>GoBarber</h1>
+      <GlobalStyles />
+    </>
+  );
 }
 
 export default App;
