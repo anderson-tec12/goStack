1a6c4d9cd71a1ac21011ddf3528f3d230f5e1cff
Anderson Barros Silva <anderson.tec12@gmail.com>
2022-01-27 23:35:43 -0300

    goBarber: implements unform in signIn

--- Nivel 3/gobarber/src/App.tsx
+++ Nivel 3/gobarber/src/App.tsx
@@ -1,12 +1,12 @@
 import GlobalStyles from "./styles/global";
 
 import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
+// import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <>
-      <SignUp />
+      <SignIn />
       <GlobalStyles />
     </>
   );
--- Nivel 3/gobarber/src/pages/SignIn/index.tsx
+++ Nivel 3/gobarber/src/pages/SignIn/index.tsx
@@ -1,16 +1,46 @@
 import { FiLogIn, FiMail, FiLock } from "react-icons/fi";
+import { Form } from "@unform/web";
 import { Container, Background, Content } from "./styles";
 import LogoImg from "../../assets/logo.svg";
-
+import * as Yup from "yup";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
+import { useCallback, useRef } from "react";
+import { FormHandles } from "@unform/core";
+import { getValidationErros } from "../../utils/getValidationErros";
 
 const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required("E-mail obrigatório")
+          .email("Digite um e-mail válido"),
+        password: Yup.string().required("Senha obrigatório"),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.dir(err);
+
+      const errors = getValidationErros(err as Yup.ValidationError);
+      formRef.current?.setErrors(errors);
+      // formRef.current?.setErrors({
+      //   name: "Nome Obrigatorio",
+      // });
+    }
+  }, []);
+
   return (
     <Container>
       <Content>
         <img src={LogoImg} alt="GoBarber" />
-        <form>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu logon</h1>
 
           <Input icon={FiMail} name="email" type="text" placeholder="E-email" />
@@ -26,7 +56,7 @@ const SignIn: React.FC = () => {
           </Button>
 
           <a href="forgot">Esqueci minha senha</a>
-        </form>
+        </Form>
         <a href="forgot">
           <FiLogIn />
           Criar conta
