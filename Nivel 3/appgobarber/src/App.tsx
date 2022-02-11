d4bf7e93b658d92008f58bca41e1b1b0abc168d0
Anderson Barros Silva <anderson.tec12@gmail.com>
2022-02-11 12:26:20 -0300

    app: config routes and navigation

--- Nivel 3/appgobarber/src/App.tsx
+++ Nivel 3/appgobarber/src/App.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
-import {View} from 'react-native';
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+import { View, StatusBar } from "react-native";
 
-const App:React.FC = () => {
-    return (
-        <View />
-    )
-}
+import { AuthRoutes } from "./routes";
 
-export default  App;
+const App: React.FC = () => {
+  return (
+    <NavigationContainer>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="#312e31"
+        hidden={false}
+      />
+      <View
+        style={{
+          backgroundColor: "#312e31",
+          flex: 1,
+        }}
+      >
+        <AuthRoutes />
+      </View>
+    </NavigationContainer>
+  );
+};
+
+export default App;
