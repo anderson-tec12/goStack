60de8686fed5c6af47f595b595bce9a32fb4877c
Anderson Barros Silva <anderson.tec12@gmail.com>
2022-02-14 08:12:19 -0300

    app:signin

--- Nivel 3/appgobarber/src/pages/SignIn/index.tsx
+++ Nivel 3/appgobarber/src/pages/SignIn/index.tsx
@@ -1,22 +1,63 @@
 import React from "react";
-import { Image } from "react-native";
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  ScrollView,
+} from "react-native";
+import Icon from "react-native-vector-icons/Feather";
 
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 
 import logoImg from "../../assets/logo.png";
-import { Container, Title } from "./styles";
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from "./styles";
 
 export const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
-      <Title>Faça seu logon</Title>
+    <>
+      <KeyboardAvoidingView
+        style={{
+          flex: 1,
+        }}
+        enabled
+        behavior={Platform.OS === "ios" ? "padding" : undefined}
+      >
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logoImg} />
 
-      <Input name="email" icon="mail" placeholder="E-mail" />
-      <Input name="password" icon="lock" placeholder="Senha" />
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
 
-      <Button onPress={() => {}}>Entrar</Button>
-    </Container>
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button onPress={() => {}}>Entrar</Button>
+
+            <ForgotPassword onPress={() => {}}>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <CreateAccountButton onPress={() => {}}>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Criar uma conta </CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   );
 };
