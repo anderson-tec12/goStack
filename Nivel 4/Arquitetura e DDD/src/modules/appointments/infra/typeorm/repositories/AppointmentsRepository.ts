de9cd3ad5773a71b794ccfe4cc5d8ed0c8e7d618
Anderson Barros Silva <anderson.tec12@gmail.com>
2022-03-01 23:30:29 -0300

    api: write repository appointments

--- Nivel 4/Arquitetura e DDD/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ Nivel 4/Arquitetura e DDD/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,21 +1,35 @@
-import { EntityRepository, Repository } from "typeorm";
+import { getRepository, Repository } from "typeorm";
 
+import ICreateAppointmentDTO from "@modules/appointments/dtos/ICreateAppointmentDTO";
 import Appointment from "../entities/Appointment";
 import IAppointmentesRepository from "@modules/appointments/repositories/IAppointmentesRepository";
 
-@EntityRepository(Appointment)
-class AppointmentsRepository
-  extends Repository<Appointment>
-  implements IAppointmentesRepository
-{
+class AppointmentsRepository implements IAppointmentesRepository {
+  private ormRepository: Repository<Appointment>;
+
+  constructor() {
+    this.ormRepository = getRepository(Appointment);
+  }
+
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const findAppointment = await this.findOne({
+    const findAppointment = await this.ormRepository.findOne({
       where: { date },
     });
 
     // const value: Appointment | null = findAppointment || null;
 
     return findAppointment;
+  }
+
+  public async create({
+    provider_id,
+    date,
+  }: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = this.ormRepository.create({ provider_id, date });
+
+    await this.ormRepository.save(appointment);
+
+    return appointment;
   }
 }
 
