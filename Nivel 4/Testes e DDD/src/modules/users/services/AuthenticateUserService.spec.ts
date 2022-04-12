13973a110977b9690c8d7aefe9b11c0dcd618964
Anderson Barros Silva <anderson.tec12@gmail.com>
2022-04-12 11:01:45 -0300

    teste de login caso o usuario nao exista

--- Nivel 4/Testes e DDD/src/modules/users/services/AuthenticateUserService.spec.ts
+++ Nivel 4/Testes e DDD/src/modules/users/services/AuthenticateUserService.spec.ts
@@ -33,4 +33,20 @@ describe("AuthenticateUser", () => {
     expect(reponse).toHaveProperty("token");
     expect(reponse.user).toEqual(user);
   });
+
+  it("should not be able to authenticate with non existing user", async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider
+    );
+
+    expect(
+      authenticateUser.execute({
+        email: "johndoe@example.com.br",
+        password: "123123",
+      })
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
